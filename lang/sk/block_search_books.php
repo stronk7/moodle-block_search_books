2cf9d8990c774b2218da1049cefd594d8e28b3b4
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-09-25 19:55:09 +0200

    sk translation. Credit goes to Ivan Breziansky. Thanks!

---
+++ lang/sk/block_search_books.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Hľadať v knihách';
+
+$string['bookssearch'] = 'Vyhľadať v knihách';
+$string['ofabout'] = 'of about';
+$string['for'] = 'pre';
+$string['norecordsfound'] = 'Nebol nájdený žiaden záznam';
+$string['results'] = 'Výsledok hľadania';
+$string['format'] = 'Zvoľte formát zobrazenia výsledku vyhľadávania';
+$string['seconds'] = 'sekúnd';
+$string['searchresults'] = 'Výsledok vyhľadávania';
+?>
